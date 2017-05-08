42bdf48b865b2c5575e82edf755a8cacfa08c8e0
sayaks <sayaks1@gmail.com>
2017-05-08 13:37:45 +0200

    i forgot to remove build

--- build/federation.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let dispatch = {};
-exports.dispatch = dispatch;
-let errorCommand = function (message, attempt) {
-    message.channel.sendMessage(attempt + " isn't a recognized federation command!");
-};
-exports.errorCommand = errorCommand;
